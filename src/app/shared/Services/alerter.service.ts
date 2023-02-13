667e005caebdfa6f4e923d1d39a2d33b84a93381
Diokou-Tech <diokoucheikh@gmail.com>
2023-02-13 22:40:07 +0000

    update before internationalization

---
+++ src/app/shared/Services/alerter.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlerterService {
+
+  // mixin
+  _toast = Swal.mixin({
+    width: 300,
+    currentProgressStep: 20,
+    toast : true,
+    timerProgressBar: true,
+    timer: 5000,
+    showConfirmButton: false,
+    position: 'bottom-end',
+    customClass: "sweetAlert",
+    background: "#FEF3C7",
+    iconColor:  "#F59E0B",
+    color: 'black',
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  });
+  _Confirm = Swal.mixin({
+    customClass: {
+      confirmButton: 'bg-green-500 mx-1',
+      cancelButton: 'bg-red-500 mx-1'
+    },
+    background: 'white',
+    buttonsStyling: false
+  });
+  constructor() { }
+
+  AlertSuccess(msg:string) : void
+  {
+    this._toast.fire("",msg,"success");
+  }
+  AlertError(msg:string) : void
+  {
+
+  }
+  ErrorServer() : void
+  {
+
+  }
+  ConfirmAction(msg:string = '') : void
+  {
+
+  }
+
+}
